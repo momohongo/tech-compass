752a46fbc20085007ed44f60e24f191c2be7bf1f
momohongo <pea13ch43@gmail.com>
2020-11-05 20:54:40 +0900

    再提出 - Javaの基礎

--- basic/src/basic/Task1.java
+++ basic/src/basic/Task1.java
@@ -3,7 +3,9 @@ package basic;
 public class Task1 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+
+		System.out.println("初めての課題");
+		System.out.println("Java攻略の道");
 
 	}
 
